9cc716c82cd2f92ff2a6a8f9835fa48853518441
31gefahr <vuralm963@gmail.com>
2026-04-23 15:19:54 +0300

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,5 +1,37 @@
 local userInventories = {} 
+local OX = exports.oxmysql
 
+
+RegisterNetEvent('envanter:esyaEkle')
+AddEventHandler('envanter:esyaEkle', function(itemName, count)
+    local src = source
+    local license = GetPlayerIdentifiers(src)[1]
+
+   
+    OX:execute('INSERT INTO user_inventory (identifier, item_name, count) VALUES (?, ?, ?) ON DUPLICATE KEY UPDATE count = count + ?', 
+    {license, itemName, count, count}, function(result)
+        TriggerClientEvent('chat:addMessage', src, {args = {"SİSTEM", count .. " adet " .. itemName .. " eklendi."}})
+    end)
+end)
+
+
+RegisterServerEvent('envanter:esyaKullan')
+AddEventHandler('envanter:esyaKullan', function(itemName)
+    local src = source
+    local license = GetPlayerIdentifiers(src)[1]
+
+    OX:scalar('SELECT count FROM user_inventory WHERE identifier = ? AND item_name = ?', {license, itemName}, function(count)
+        if count and count > 0 then
+            
+            OX:execute('UPDATE user_inventory SET count = count - 1 WHERE identifier = ? AND item_name = ?', {license, itemName})
+            
+          
+            TriggerClientEvent('envanter:efektTetikle', src, itemName)
+        else
+            TriggerClientEvent('chat:addMessage', src, {args = {"HATA", "Üzerinde bu eşyadan yok!"}})
+        end
+    end)
+end)
 
 RegisterNetEvent('inventory:addItem')
 AddEventHandler('inventory:addItem', function(itemName, count)
