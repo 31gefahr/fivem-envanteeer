39ab533f5b9b44e8ea89412527a7442304281d2e
31gefahr <vuralm963@gmail.com>
2026-04-23 15:37:04 +0300

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,5 +1,38 @@
 RegisterKeyMapping('envanter', 'Envanter Arayüzünü Aç', 'keyboard', 'F2')
+RegisterKeyMapping('openinv', 'Envanteri Aç', 'keyboard', 'F2')
 
+RegisterCommand('openinv', function()
+    local ped = PlayerPedId()
+    local veh = GetVehiclePedIsIn(ped, false)
+    local targetInv = "ground" 
+
+    if veh ~= 0 then
+        targetInv = "glovebox" 
+    else
+        local closeVeh = GetClosestVehicle(GetEntityCoords(ped), 3.0, 0, 71)
+        if DoesEntityExist(closeVeh) then
+           
+            SetVehicleDoorOpen(closeVeh, 5, false, false)
+            targetInv = "trunk"
+        end
+    end
+
+   
+    SetNuiFocus(true, true)
+    SendNUIMessage({
+        action = "display",
+        type = targetInv,
+        maxWeight = 100
+    })
+end)
+
+
+for i = 1, 5 do
+    RegisterKeyMapping('slot_'..i, 'Slot '..i, 'keyboard', tostring(i))
+    RegisterCommand('slot_'..i, function()
+        TriggerServerEvent('inventory:useHotkey', i)
+    end)
+end
 
 RegisterCommand('su-al', function()
     local itemName = "su"
