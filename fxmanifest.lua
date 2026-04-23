9c822eeb0700bc40c1295be86a20a3fe32b82ca9
31gefahr <vuralm963@gmail.com>
2026-04-23 15:40:36 +0300

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,12 +2,30 @@ fx_version 'cerulean'
 game 'gta5'
 
 author 'Geliştirici'
-description 'Basit Envanter Sistemi'
+description 'Envanter Sistemi'
+
 
 server_scripts {
+    '@oxmysql/lib/MySQL.lua', 
     'server.lua'
 }
 
+
 client_scripts {
     'client.lua'
+}
+
+
+ui_page 'ui/index.html'
+
+files {
+    'ui/index.html',
+    'ui/style.css',
+    'ui/script.js',
+    'ui/img/*.png' 
+}
+
+
+capabilities {
+    'unstable'
 }
