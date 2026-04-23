e8dea3ab7e5777ff2585647f12be53128882e874
31gefahr <vuralm963@gmail.com>
2026-04-23 15:21:12 +0300

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,3 +1,6 @@
+RegisterKeyMapping('envanter', 'Envanter Arayüzünü Aç', 'keyboard', 'F2')
+
+
 RegisterCommand('su-al', function()
     local itemName = "su"
     local count = 1
@@ -5,12 +8,25 @@ RegisterCommand('su-al', function()
 end)
 
 
-Citizen.CreateThread(function()
-    while true do
-        Citizen.Wait(0)
-        if IsControlJustReleased(0, 289) then 
-            
-            ExecuteCommand('envanter')
-        end
+RegisterNetEvent('envanter:efektTetikle')
+AddEventHandler('envanter:efektTetikle', function(itemName)
+    local playerPed = PlayerPedId()
+
+    if itemName == "bandaj" then
+        
+        TaskStartScenarioInPlace(playerPed, "WORLD_HUMAN_MEDIC_TEND_TO_KNOT", 0, true)
+        
+      
+        Citizen.SetTimeout(5000, function()
+            ClearPedTasks(playerPed)
+            local currentHealth = GetEntityHealth(playerPed)
+            SetEntityHealth(playerPed, currentHealth + 20)
+            print("Bandaj kullandın, canın yenilendi.")
+        end)
+        
+    elseif itemName == "ekmek" then
+        
+        print("Yemek yedin, açlığın azaldı.")
+       
     end
 end)
